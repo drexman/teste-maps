933c43784d644ef234c88ca637ae94931380ac26
jemluz <contatojemima@gmail.com>
2023-10-16 17:37:08 -0300

    add state

--- src/ReactGoogleMapsAPI/ReactGoogleMapsAPI.tsx
+++ src/ReactGoogleMapsAPI/ReactGoogleMapsAPI.tsx
@@ -1,6 +1,6 @@
 import { GoogleMap, Polygon, useLoadScript } from "@react-google-maps/api"
 import './styles.css'
-// import { useMemo } from "react";
+import { useState } from "react";
 
 const apiKey: string = 'AIzaSyA-qm42bToshX0BMcK_57V33l9omMszx8U';
 
@@ -13,58 +13,61 @@ export function ReactGoogleMapsAPI() {
   return <MapWithPolygon />;
 }
 
-// function MapWithMarker() {
-//   const centerPosition = useMemo(() => ({ lat: 44, lng: -80 }), []);
+interface Coordinates {
+  lat: number;
+  lng: number;
+}
 
-//   return (
-//     <GoogleMap
-//       zoom={10}
-//       center={centerPosition}
-//       mapContainerClassName="map-container"
-//     >
-//       <Marker position={centerPosition} />
-//     </GoogleMap>
-//   )
-// }
+const polygonOptions = {
+  fillColor: "#00000020",
+  fillOpacity: 1,
+  strokeColor: "#FF0000",
+  strokeOpacity: .15,
+  strokeWeight: 1,
+  clickable: true,
+  draggable: true,
+  editable: true,
+  geodesic: false,
+  visible: true,
+  zIndex: 1
+} as const
+
 
 function MapWithPolygon() {
-  const center = { lat: 24.886, lng: -70.268 }
-
-  const paths = [
+  const initialCenter: Coordinates = { lat: 24.886, lng: -70.268 };
+  const initialPoints: Coordinates[] = [
     { lat: 25.774, lng: -80.19 },
     { lat: 18.466, lng: -66.118 },
     { lat: 32.321, lng: -64.757 },
     { lat: 25.774, lng: -80.19 }
   ]
 
-  const options = {
-    fillColor: "lightblue",
-    fillOpacity: 1,
-    strokeColor: "red",
-    strokeOpacity: 1,
-    strokeWeight: 2,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    geodesic: false,
-    zIndex: 1
-  }
+  const [center] = useState<Coordinates>(initialCenter);
+  const [points] = useState<Coordinates[]>(initialPoints);
 
   const onLoad = (polygon: object) => {
     console.log("polygon: ", polygon);
+  }
+
+  const onEdit = (data: object) => {
+    console.log(data)
   }
 
   return (
     <GoogleMap
       id="marker-example"
       mapContainerClassName="map-container"
-      zoom={5}
+      zoom={6}
       center={center}
     >
       <Polygon
         onLoad={onLoad}
-        paths={paths}
-        options={options}
+        paths={points}
+        options={polygonOptions}
+        onDrag={onEdit}
+        onClick={onEdit}
+        editable
+        draggable
       />
     </GoogleMap>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { ReactGoogleMapsAPI } from './ReactGoogleMapsAPI/ReactGoogleMapsAPI.tsx'
+import { App } from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ReactGoogleMapsAPI />
-  </React.StrictMode>,
+  <App />
 )
