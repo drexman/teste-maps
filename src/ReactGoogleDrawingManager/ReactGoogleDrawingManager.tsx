c050ec1ea52761fe4e7bb2076cd5680d46352380
Samuel Oizume <samuel.oizume-ext2@ab-inbev.com>
2023-10-18 16:43:41 -0300

    update

--- src/ReactGoogleDrawingManager/ReactGoogleDrawingManager.tsx
+++ src/ReactGoogleDrawingManager/ReactGoogleDrawingManager.tsx
@@ -1,6 +1,7 @@
-import { GoogleMap, DrawingManager, useLoadScript, LoadScript} from "@react-google-maps/api"
+import { GoogleMap, DrawingManager, useLoadScript } from "@react-google-maps/api"
 import './styles.css'
 import { useState } from "react";
+
 
 const apiKey: string = 'AIzaSyA-qm42bToshX0BMcK_57V33l9omMszx8U';
 
@@ -19,7 +20,7 @@ export function ReactGoogleDrawingManager() {
   return <MapWithDrawingManager />;
 }
 
-function MapWithDrawingManager () {
+function MapWithDrawingManager() {
 
   const [polys, setPolys] = useState<Array<Coordinate>>([]);
 
@@ -30,20 +31,20 @@ function MapWithDrawingManager () {
     strokeWeight: 2,
     draggable: true,
     editable: true
-}
+  }
 
-const drawingManagerOptions = {
+  const drawingManagerOptions = {
     drawingMode: polygonOptions,
     drawingControl: true,
     drawingControlOptions: {
-        position: window.google?.maps?.ControlPosition?.TOP_CENTER,
-        drawingModes: [
-          google.maps.drawing.OverlayType.MARKER,
-            google.maps.drawing.OverlayType.CIRCLE,
-            google.maps.drawing.OverlayType.POLYGON,
-            google.maps.drawing.OverlayType.POLYLINE,
-            google.maps.drawing.OverlayType.RECTANGLE
-        ]
+      position: window.google?.maps?.ControlPosition?.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.POLYGON,
+        google.maps.drawing.OverlayType.POLYLINE,
+        google.maps.drawing.OverlayType.RECTANGLE
+      ]
     },
     circleOptions: {
       fillColor: 'rgb(216, 191, 255)',
@@ -62,41 +63,54 @@ const drawingManagerOptions = {
       strokeWeight: 2,
       draggable: true,
       editable: true
-      
+
     }
 
 
-}
+  }
 
   const center = { lat: 52.52047739093263, lng: 13.36653284549709 }
 
-  const onLoad = (d : any) => {
-    console.log(d);
-  }
+  const onPolygonComplete = (polygon: google.maps.Polygon) => {
+    const polyArray = polygon.getPath().getArray();
 
-  const onPolygonComplete = (polygon : google.maps.Polygon) => {
-    const polyArray =  polygon.getPath().getArray();
-    let va = [];
-    
-    polyArray.forEach((path: google.maps.LatLng) => {
-      va.push({ lat: path.lat(), lng: path.lng });
-    }) 
-
-    console.log(paths);
+    console.log(polyArray);
   }
 
   return (
-    <GoogleMap
-      id="marker-example"
-      mapContainerClassName="map-container"
-      zoom={12}
-      center={center}
-    >
-      <DrawingManager
-        onPolygonComplete={onPolygonComplete}
-        options={drawingManagerOptions}
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      height: '100vh',
+      width: '100vw' 
+    }}>
+      <div style={{ 
+        position:'absolute',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0,
+      }}>
+      <GoogleMap
+        id="marker-example"
+        mapContainerClassName="map-container"
+        zoom={12}
+        center={center}
+      >
+        <DrawingManager
+          onPolygonComplete={onPolygonComplete}
+          options={drawingManagerOptions}
         />
-
-    </GoogleMap>
+      </GoogleMap>
+      </div>
+      <div style={{ 
+        backgroundColor: '#FFF',
+        color: '#000',
+        height: '100px',
+        zIndex: 1
+      }}>
+        <button onClick={ () => alert('teste')}>Button</button>
+      </div>
+    </div>
   )
 }
