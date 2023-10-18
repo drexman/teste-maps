d409a35490e3d1670b11ffe03da81bb7eed1c61d
Nathan Duarte <nathan.duarte-ext@ab-inbev.com>
2023-10-18 18:05:12 -0300

    New features

--- src/ReactGoogleDrawingManager/ReactGoogleDrawingManager.tsx
+++ src/ReactGoogleDrawingManager/ReactGoogleDrawingManager.tsx
@@ -1,41 +1,92 @@
-import { GoogleMap, DrawingManager, useLoadScript } from "@react-google-maps/api"
-import './styles.css'
+import {
+  GoogleMap,
+  DrawingManager,
+  useLoadScript,
+  Polygon,
+  Marker,
+} from "@react-google-maps/api";
+import "./styles.css";
 import { useState } from "react";
+import ReactDOMServer from 'react-dom/server';
+// import { StoreIcon } from "../assets/StoreIcon";
 
+import StoreIcon from "../assets/Store_icon.svg"
 
-const apiKey: string = 'AIzaSyA-qm42bToshX0BMcK_57V33l9omMszx8U';
+const apiKey: string = "AIzaSyA-qm42bToshX0BMcK_57V33l9omMszx8U";
+
+/**
+ * Creates a circular area around a coordinate
+ * @param { Array<number> } center Central point
+ * @param { string | number } radius Radius of the area in Km. Ex: 9
+ * @param { number } corners Number of points of the circle (the more corners, more rounded the circle will be, but more coordinates will have)
+ * @returns Array with an array of numbers with a pair of coordinates, indicating longitude and latitude
+ * @example [[long, lat], [long, lat], [long, lat]]
+ */
+export const createCircularArea = (
+  center: Coordinate,
+  radius: string | number,
+  corners = 30
+): Array<Coordinate> => {
+  const sellerArea = [];
+  const xCoords = [];
+  const yCoords = [];
+  const formattedRadius = Number(radius) / 100;
+  const centerX = center.lat;
+  const centerY = center.lng;
+
+  // Generate the corrdinates x and y
+  for (let i = 0; i < corners; i++) {
+    xCoords.push(
+      +Number(
+        centerX + formattedRadius * Math.cos(2 * Math.PI * (i / corners))
+      ).toFixed(7)
+    );
+    yCoords.push(
+      +Number(
+        centerY + formattedRadius * Math.sin(2 * Math.PI * (i / corners))
+      ).toFixed(7)
+    );
+  }
+
+  // Add the coordinates pair in only one location
+  for (let i = 0; i < xCoords.length; i++) {
+    sellerArea.push({ lat: xCoords[i], lng: yCoords[i] });
+  }
+
+  // Copy the first coordinate to final position to close the circle
+  sellerArea.push(sellerArea[0]);
+  return sellerArea;
+};
 
 export type Coordinate = {
-  lat: number,
-  lng: number,
-}
+  lat: number;
+  lng: number;
+};
 
 export function ReactGoogleDrawingManager() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: apiKey,
     libraries: ["drawing"],
-  })
+  });
 
   if (!isLoaded) return <div>Loading...</div>;
   return <MapWithDrawingManager />;
 }
 
 function MapWithDrawingManager() {
-
   const [polys, setPolys] = useState<Array<Coordinate>>([]);
 
-  const polygonOptions = {
-    fillOpacity: 0.3,
-    fillColor: 'rgb(216, 191, 255)',
-    strokeColor: 'rgb(47, 0, 114)',
+  const polygonOptions: google.maps.PolygonOptions = {
+    fillColor: "rgba(216,191,255,0.5)",
+    strokeColor: "#2F0072",
     strokeWeight: 2,
-    draggable: true,
-    editable: true
-  }
+    draggable: false,
+    editable: false,
+  };
 
-  const drawingManagerOptions = {
-    drawingMode: polygonOptions,
-    drawingControl: true,
+  const drawingManagerOptions: google.maps.drawing.DrawingManagerOptions = {
+    // drawingMode: polygonOptions,
+    drawingControl: false,
     drawingControlOptions: {
       position: window.google?.maps?.ControlPosition?.TOP_CENTER,
       drawingModes: [
@@ -43,74 +94,104 @@ function MapWithDrawingManager() {
         google.maps.drawing.OverlayType.CIRCLE,
         google.maps.drawing.OverlayType.POLYGON,
         google.maps.drawing.OverlayType.POLYLINE,
-        google.maps.drawing.OverlayType.RECTANGLE
-      ]
+        google.maps.drawing.OverlayType.RECTANGLE,
+      ],
     },
     circleOptions: {
-      fillColor: 'rgb(216, 191, 255)',
+      fillColor: "rgb(216, 191, 255)",
       fillOpacity: 0.3,
       strokeWeight: 2,
-      strokeColor: 'rgb(47, 0, 114)',
+      strokeColor: "rgb(47, 0, 114)",
       clickable: true,
       editable: true,
-
     },
-
     polygonOptions: {
       fillOpacity: 0.3,
-      fillColor: 'rgb(216, 191, 255)',
-      strokeColor: 'rgb(47, 0, 114)',
+      fillColor: "rgb(216, 191, 255)",
+      strokeColor: "rgb(47, 0, 114)",
       strokeWeight: 2,
       draggable: true,
-      editable: true
+      editable: true,
+    },
+  };
 
-    }
-
-
-  }
-
-  const center = { lat: 52.52047739093263, lng: 13.36653284549709 }
+  const center = { lat: 13.72311, lng: -89.20623 };
 
   const onPolygonComplete = (polygon: google.maps.Polygon) => {
     const polyArray = polygon.getPath().getArray();
 
     console.log(polyArray);
-  }
+  };
+
+  const renderMarker = () => {
+    const priceTag = document.createElement("div");
+
+    return priceTag
+  };
 
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      height: '100vh',
-      width: '100vw' 
-    }}>
-      <div style={{ 
-        position:'absolute',
-        width: '100%',
-        height: '100%',
-        top: 0,
-        left: 0,
-      }}>
-      <GoogleMap
-        id="marker-example"
-        mapContainerClassName="map-container"
-        zoom={12}
-        center={center}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+        }}
       >
-        <DrawingManager
-          onPolygonComplete={onPolygonComplete}
-          options={drawingManagerOptions}
-        />
-      </GoogleMap>
+        <GoogleMap
+          id="marker-example"
+          mapContainerClassName="map-container"
+          zoom={12}
+          center={center}
+          options={{
+            fullscreenControl: false,
+            mapTypeControl: false,
+            streetViewControl: false,
+          }}
+        >
+          <DrawingManager
+            onPolygonComplete={onPolygonComplete}
+            options={drawingManagerOptions}
+          />
+
+          <Marker
+            position={center}
+            icon={{
+              url: StoreIcon,
+            }}
+          />
+
+          <Polygon
+            paths={createCircularArea(center, 9, 30)}
+            options={polygonOptions}
+            onDrag={console.log}
+            onClick={console.log}
+            editable
+            draggable
+          />
+        </GoogleMap>
       </div>
-      <div style={{ 
-        backgroundColor: '#FFF',
-        color: '#000',
-        height: '100px',
-        zIndex: 1
-      }}>
-        <button onClick={ () => alert('teste')}>Button</button>
+      <div
+        style={{
+          height: "100px",
+        }}
+      >
+        <button style={{
+          color: "#000",
+          // height: "100px",
+          zIndex: 1,
+        }}
+        onClick={() => alert("teste")}>Button</button>
       </div>
     </div>
-  )
+  );
 }
