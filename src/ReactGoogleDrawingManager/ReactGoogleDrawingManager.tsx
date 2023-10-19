7cd5cd50a4ab90004341960ce0438f77e0831fed
Nathan Duarte <nathan.duarte-ext@ab-inbev.com>
2023-10-19 13:45:44 -0300

    Slider radius component

--- src/ReactGoogleDrawingManager/ReactGoogleDrawingManager.tsx
+++ src/ReactGoogleDrawingManager/ReactGoogleDrawingManager.tsx
@@ -6,21 +6,18 @@ import {
   Marker,
 } from "@react-google-maps/api";
 import "./styles.css";
-import { useState } from "react";
-import ReactDOMServer from 'react-dom/server';
-// import { StoreIcon } from "../assets/StoreIcon";
-
-import StoreIcon from "../assets/Store_icon.svg"
+import { useEffect, useState } from "react";
+import StoreIcon from "../assets/Store_icon.svg";
 
 const apiKey: string = "AIzaSyA-qm42bToshX0BMcK_57V33l9omMszx8U";
 
 /**
  * Creates a circular area around a coordinate
- * @param { Array<number> } center Central point
+ * @param { Coordinate } center Central point
  * @param { string | number } radius Radius of the area in Km. Ex: 9
  * @param { number } corners Number of points of the circle (the more corners, more rounded the circle will be, but more coordinates will have)
  * @returns Array with an array of numbers with a pair of coordinates, indicating longitude and latitude
- * @example [[long, lat], [long, lat], [long, lat]]
+ * @example [{lat: xxx, lng: xxx}], {lat: xxx, lng: xxx}, {lat: xxx, lng: xxx}]
  */
 export const createCircularArea = (
   center: Coordinate,
@@ -76,6 +73,8 @@ export function ReactGoogleDrawingManager() {
 function MapWithDrawingManager() {
   const [polys, setPolys] = useState<Array<Coordinate>>([]);
   const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [radius, setRadius] = useState<string>('9');
+  const center = { lat: 13.72311, lng: -89.20623 };
 
   const polygonOptions: google.maps.PolygonOptions = {
     fillColor: "rgba(216,191,255,0.5)",
@@ -116,48 +115,41 @@ function MapWithDrawingManager() {
     },
   };
 
-  const center = { lat: 13.72311, lng: -89.20623 };
+  useEffect(() => {
+    createCircule(radius)
+  }, [radius])
 
   const onPolygonComplete = (polygon: google.maps.Polygon) => {
     const polyArray = polygon.getPath().getArray();
-
-    console.log(polyArray);
+    console.log(polyArray)
   };
 
-  const renderMarker = () => {
-    const priceTag = document.createElement("div");
-
-    return priceTag
+  const createCircule = (currentRadius: string) => {
+    setPolys(createCircularArea(center, currentRadius));
   };
-
-  const createCircule = () => {
-    setPolys(createCircularArea(center, 9, 30));
-  }
 
   const handleChange = (event: any) => {
     const val = event.target.value;
-    if(val === 'view') {
+    if (val === "view") {
       setIsEdit(false);
-    } else if(val === 'edit') {
+    } else if (val === "edit") {
       setIsEdit(true);
     }
-  } 
+  };
 
   return (
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
+        flexDirection: "row",
+        justifyContent: 'flex-start',
         height: "100vh",
         width: "100vw",
-        padding: '20px',
       }}
     >
       <div
         style={{
           position: "absolute",
-          width: "100%",
-          height: "100%",
           top: 0,
           left: 0,
         }}
@@ -195,31 +187,59 @@ function MapWithDrawingManager() {
           />
         </GoogleMap>
       </div>
+
       <div
         style={{
-          backgroundColor: '#F3F3F3',
-          opacity: 0.9,
-          height: "300px",
-          width: "150px",
+          backgroundColor: "white",
+          height: "200px",
+          width: "200px",
+          borderRadius: '10px',
           zIndex: 1,
-          padding: '5px',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center'
+          padding: "16px",
+          display: "flex",
+          margin: '16px',
+          flexDirection: "column",
+          justifyContent: "center",
         }}
       >
-        <button style={{
-          color: "#000",
-        }}
-          onClick={createCircule}>Criar nova area
-          </button>
-        <div style={{ marginTop:  '10px' }}>
-        <input type="radio" id="view" name="mode" value="view" onChange={handleChange}/><label style={{ color: 'black', fontSize: 11 }}>Visualizar</label>
+        <button
+          style={{
+            backgroundColor: 'white',
+            borderRadius: '4px',
+            height: '32px',
+            color: "#000",
+            cursor: 'pointer'
+          }}
+          onClick={() => createCircule('9')}
+        >
+          Criar nova area
+        </button>
+        <div style={{ marginTop: "10px" }}>
+          <input
+            type="radio"
+            id="view"
+            name="mode"
+            value="view"
+            defaultChecked
+            onChange={handleChange}
+          />
+          <label style={{ color: "black", fontSize: 11 }}>Visualizar</label>
         </div>
         <div>
-        <input type="radio" id="edit" name="mode" value="edit" onChange={handleChange}/><label style={{ color: 'black', fontSize: 11}}>Modo edição</label>
+          <input
+            type="radio"
+            id="edit"
+            name="mode"
+            value="edit"
+            onChange={handleChange}
+          />
+          <label style={{ color: "black", fontSize: 11 }}>Modo edição</label>
         </div>
 
+        <div>
+          <input type="range" step={1}  min={1} max={15} value={radius} onChange={(e) => setRadius(e.target.value)} />
+          <label style={{ color: "black", fontSize: 11 }}>{radius}</label>
+        </div>
       </div>
     </div>
   );
